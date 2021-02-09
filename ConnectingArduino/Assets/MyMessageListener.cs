d68c1345e7e9067766c7a3d505cad0ec33e977e9
brad-lei <zhenhong_lei@brown.edu>
2021-02-09 12:27:58 +0800

    p

---
+++ ConnectingArduino/Assets/MyMessageListener.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MyMessageListener : MonoBehaviour
+{
+    [SerializeField]
+    GameObject spinner;
+    // Use this for initialization
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    // Invoked when a line of data is received from the serial device.
+    void OnMessageArrived(string msg)
+    {
+        Debug.Log("Arrived: " + msg);
+        //spinner.transform.Translate(Vector3.forward * Time.deltaTime * float.Parse(msg) * 10);
+        spinner.transform.Rotate(0f, Time.deltaTime * float.Parse(msg) * 10, 0f, Space.World);
+    }
+    // Invoked when a connect/disconnect event occurs. The parameter 'success'
+    // will be 'true' upon connection, and 'false' upon disconnection or
+    // failure to connect.
+    void OnConnectionEvent(bool success)
+    {
+        Debug.Log(success ? "Device connected" : "Device disconnected");
+    }
+}
